18aaa3f8a5312f41ebd430a3c26c152ec5cc32d0
Rodrigo Ropelato <rodrigoropelato@hotmail.com>
2023-11-15 23:00:49 -0300

    desafio GitFav finalizado para entrga

---
+++ js/Favorites.js
@@ -0,0 +1,149 @@
+import { GithubUser } from "./GithubUser.js";
+
+export class Favorites {
+    constructor(root) {
+        this.root = document.querySelector(root)
+        this.load()
+    }
+
+    load() {
+        this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+    }
+
+    save() {
+        localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
+    }
+
+    async add(username) {
+        console.log(username)
+        try {
+            const userExists = this.entries.find(entry => entry.login.toLowerCase() === username.toLowerCase())
+
+            if(userExists) {
+                throw new Error('Usuário já cadastrado')
+            }
+
+            const user = await GithubUser.search(username)
+
+            if(user.login === undefined) {
+                throw new Error('Usuário não encontrado!')
+            }
+
+            this.entries = [user, ...this.entries]
+            this.update()
+            this.save()
+        }
+        catch (error) {
+            alert(error.message)
+        }
+    }
+
+    delete(user) {
+        const filteredEntries = this.entries.filter(entry =>
+            entry.login !== user.login
+        )
+
+        this.entries = filteredEntries
+
+        this.update()
+        this.save()
+    }
+}
+
+export class FavoritesView extends Favorites {
+    constructor(root) {
+        super(root)
+
+        this.tbody = this.root.querySelector('table tbody')
+
+        this.update()
+        this.onadd()
+    }
+
+    onadd() {
+        const addButton = this.root.querySelector('.search button')
+        addButton.onclick = () => {
+            const { value } = this.root.querySelector('.search input')
+            this.add(value)
+        }
+    }
+
+    update() {
+        this.removeAllTr()
+
+        this.entries.forEach(user => {
+            const row = this.createRow()
+
+            row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+            row.querySelector('.user img').alt = `Imagem de ${user.name}`
+            row.querySelector('.user p').textContent = user.name
+            row.querySelector('.user span').textContent = `/${user.login}`
+            row.querySelector('.user a').href = user.html_url
+            row.querySelector('.repositories').textContent = user.public_repos
+            row.querySelector('.followers').textContent = user.followers
+
+            row.querySelector('.remove').onclick = () => {
+                const isOk = confirm('Tem certeza que deseja deletar essa linha?')
+
+                if (isOk) {
+                    this.delete(user)
+                }
+            }
+
+            this.tbody.append(row)
+        })
+
+        if(this.entries[0] === undefined){
+            const row = this.createEmptyRow()
+            row.querySelector('.no-content')
+            this.tbody.append(row)
+        } 
+    }
+
+    createRow() {
+        const tr = document.createElement('tr')
+
+        tr.innerHTML = `
+        
+            <td class="user">
+                <img src="https://github.com/rodrigoropelato.png" alt="Imagem de rodrigoropelato">
+                <a href="https://github.com/rodrigoropelato" target="_blank">
+                    <p>Rodrigo Ropelato</p>
+                    <span>/rodrigoropelato</span>
+                </a>
+            </td>
+            <td class="repositories">
+                42
+            </td>
+            <td class="followers">
+                3
+            </td>
+            <td>
+                <button class="remove">Remover</button>
+            </td>
+        `
+        return tr
+
+    }
+
+    createEmptyRow(){
+        const tr = document.createElement('tr')
+
+        tr.innerHTML = `
+        <td class="no-content" colspan="4">
+            <div class="flex-container">
+                <img src="./assets/Star.svg" alt="">
+                <h1>Nenhum favorito ainda</h1>
+            </div>
+        </td>
+        `
+        return tr
+    }
+
+    removeAllTr() {
+        this.tbody.querySelectorAll('tr')
+            .forEach((tr) => {
+                tr.remove()
+            });
+    }
+}
